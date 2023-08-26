64b007983ccee05ff608ec8b96514e9c878b3695
fartem <jaman.smlnsk@gmail.com>
2023-08-26 21:17:27 +0300

    2023-08-26 v. 0.0.1: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# serverio_rb_flutter
+
+[serverio-rb](https://github.com/serverio-rb/serverio-rb) mobile client.
---
+++ android/app/src/main/kotlin/com/example/serverio_rb_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.serverio_rb_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
